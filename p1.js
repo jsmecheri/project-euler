348534ecb4407db659c0be3b3d06753652c5c95a
Ciprian Tepes <Ciprian Tepes>
2016-07-26 13:17:31 +0100

    Refactor p1.js

--- p1.js
+++ p1.js
@@ -1,13 +1,19 @@
-function tot(n) {
-  if ( n > 0 ) {
-    if (n%3===0||n%5===0) {
-//      console.log(n);
-      return tot(n-1)+n;
-    } 
-    return tot(n-1);
+var arg = 10;
+function tot ( n ) {
+  if ( n <= 0 ) {
+    return 0;
   }
-  return n;
+  if ( n%3 === 0 || n %5 === 0 ) {
+    return tot( n-1 ) + n;
+  } 
+  return tot( n-1 );
 }
+//if called from node.js, please supply argument
 //console.log(process.argv);
-console.log(tot(parseInt(process.argv[2])));
+
+//Provide default argument if not called from nodejs
+if ( typeof parseInt( process.argv[ 2 ] ) === "number" ) {
+  arg = parseInt( process.argv[ 2 ] );
+}
+console.log( tot( arg ) );
 //9,8,7,6,5,4,3,2,1
